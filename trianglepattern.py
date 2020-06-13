ab8921b81c1100991d23d4fc0207e243ddf92c7f
StormBreaker <31597693+yash-saini@users.noreply.github.com>
2020-06-13 13:34:50 +0530

    Add files via upload

---
+++ trianglepattern.py
@@ -0,0 +1,26 @@
+x=input('enter the lines')
+for i in range(x,0,-1):
+    for k in range(i-1):
+        print '',
+    while i<x+1:
+        print '*',
+        i=i+1
+    print
+
+for i in xrange(x,0,-1):
+    for k in range(i-1):
+        print '',
+    while i<x+1:
+        print '*',
+        i=i+1
+    print
+for i in xrange(x,0,-1):
+    for k in range(i-1):
+        print '',
+    while i<x+1:
+        print '*',
+        i=i+1
+    print
+for i in range(x):
+    print ' '*(x-3),'|||'
+print '====_____============================='
